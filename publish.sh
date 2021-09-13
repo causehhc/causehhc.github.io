2dc995ba9a46fb57a347faf01cf70beacc10e455
causehhc <hhc404404@gamil.com>
2021-09-13 20:57:17 +0800

    commit

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@ hugo -d docs &&
 git init && 
 git add . && 
 git commit -m "commit" && 
-git remote add origin https://github.com/causehhc/causehhc.github.io.git && 
+# git remote add origin https://github.com/causehhc/causehhc.github.io.git && 
 git push -u origin master && 
 echo "Done"
